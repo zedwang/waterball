ca443b808b7606d4dbb1bc1bbd69b14dcc0bcd10
zed <343650735@qq.com>
2017-08-13 21:37:55 +0800

    add test framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
 /**
  * Created by Zed on 2017/8/2.
  */
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const del = require('del');
+const $ = gulpLoadPlugins();
+
+
+
+gulp.task('test',()=> {
+
+})
+
+gulp.task('build',()=>{
+
+})
+
+gulp.task('default',['build']);
---
+++ test/sepc.test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/8/13 0013.
+ */
+(function () {
+    'use strict';
+
+    describe('Give it some context', function () {
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
+
+            });
+        });
+    });
+})();
