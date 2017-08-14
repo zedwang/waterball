964b2e4fcad4bc7dd3d1f5ae20859bc1241ef326
zed <343650735@qq.com>
2017-08-14 22:20:43 +0800

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,35 @@ const $ = gulpLoadPlugins();
 gulp.task('test',()=> {
 
 })
+function lint(files, options) {
+    return gulp.src(files)
+        .pipe(reload({stream: true, once: true}))
+        .pipe($.eslint(options))
+        .pipe($.eslint.format())
+        .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+}
 
-gulp.task('build',()=>{
+gulp.task('lint', () => {
+    return lint('src/**/*.js', {
+        fix: true
+    })
+        .pipe(gulp.dest('src'));
+});
+gulp.task('lint:test', () => {
+    return lint('test/spec/**/*.js', {
+        fix: true,
+        env: {
+            mocha: true
+        }
+    })
+        .pipe(gulp.dest('test/spec/**/*.js'));
+});
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-})
+gulp.task('build', ['lint'], () => {
+    return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
 
-gulp.task('default',['build']);
+gulp.task('default', ['clean'], () => {
+    gulp.start('build');
+});
